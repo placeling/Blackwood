539ae09dfb53fdc8af7f372fd4c3c8e17d3d8b3e
imackinn <imackinn@alumni.uwaterloo.ca>
2012-11-19 10:29:12 -0800

    Initial commit of generated app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackwood::Application.config.secret_token = '8ec9aac671a25e67f12ddb3be24488a64c95dd67fd80c84a76e88d5e031d7168a80df178960efeaf8e9241110dde401435434f3788b02638c63ea1fd961b0381'
