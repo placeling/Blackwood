01a6d924eda2af036d46cfae2761219ac7265b19
imackinn <imackinn@alumni.uwaterloo.ca>
2012-11-21 11:57:42 -0800

    add basic deployment script that assumes same instance as chatham

---
+++ config/deploy.rb
@@ -0,0 +1,63 @@
+require 'bundler/capistrano'
+
+set :application, "blackwood"
+
+set :rvm_ruby_string, "ruby-1.9.3-p125"
+require "rvm/capistrano" # Load RVM's capistrano plugin.
+
+before 'deploy:setup', 'ubuntu:required_packages'
+before 'deploy:setup', 'rvm:install_rvm'
+before 'deploy:setup', 'rvm:install_ruby'
+
+task :production do
+  set :gateway, 'beagle.placeling.com:11235'
+  server '10.112.241.90', :app, :web, :db, :scheduler, :primary => true
+  ssh_options[:forward_agent] = true #forwards local-localhost keys through gateway
+  set :user, 'ubuntu'
+  set :use_sudo, false
+  set :rails_env, "production"
+end
+
+task :staging do
+  server 'staging.placeling.com', :app, :web, :db, :scheduler, :primary => true
+  ssh_options[:forward_agent] = true
+  set :deploy_via, :remote_cache
+  set :user, 'ubuntu'
+  set :port, '11235'
+  set :use_sudo, false
+  set :rails_env, "staging"
+end
+
+default_run_options[:pty] = true # Must be set for the password prompt from git to work
+set :repository, "git@github.com:placeling/Blackwood.git" # Your clone URL
+set :scm, "git"
+
+set :deploy_to, "/var/www/apps/#{application}"
+set :shared_directory, "#{deploy_to}/shared"
+set :deploy_via, :remote_cache
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+
+  desc "Restart Resque Workers"
+  task :restart_workers, :roles => :app do
+    run_remote_rake "resque:restart_workers"
+  end
+
+  desc "Restart Resque scheduler"
+  task :restart_scheduler, :roles => :scheduler do
+    run_remote_rake "resque:restart_scheduler"
+  end
+
+end
