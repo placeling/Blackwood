ae64140b325f4fb999dc0199d5842cd9d143b468
imackinn <imackinn@alumni.uwaterloo.ca>
2012-11-26 12:40:34 -0800

    add rpsec placeholder test

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+
+describe "Home Controller" do
+
+  it "should display index" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == true
+  end
+end
