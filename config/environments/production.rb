d9162f8e407a1e158e201b96e8b5fdb7358a091e
imackinn <imackinn@alumni.uwaterloo.ca>
2012-11-27 19:16:18 -0800

    try using cdn for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Blackwood::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://d2wdesbogsm8l5.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
